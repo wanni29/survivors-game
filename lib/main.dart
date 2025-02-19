6504e668bb9975cdc531d5ea7d38453c9f2e65da
wanni30 <hubhy@nate.com>
2025-02-20 01:16:57 +0900

    배경, 이동, 캐릭터, 적 생성 완료 / 충돌 모션, 캐릭터 회피기 구현하기

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,80 @@
+import 'package:flame/components.dart';
+import 'package:flame/events.dart';
+import 'package:flame/game.dart';
+import 'package:flame/input.dart';
+import 'package:flame/parallax.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(GameWidget(game: MyGame()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyGame extends FlameGame with PanDetector, KeyboardEvents {
+  late SpriteComponent player;
+  late SpriteComponent enermy;
+  Vector2 moveDirection = Vector2.zero();
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+  Future<void> onLoad() async {
+    // 배경 추가하기
+    final parallax = await loadParallaxComponent(
+      [ParallaxImageData('background.jpg')],
+      baseVelocity: Vector2(50, 0), // 천천히 스크롤되는 배경
+      repeat: ImageRepeat.repeat,
     );
-  }
-}
+    add(parallax);
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+    // 캐릭터 추가하기
+    player = SpriteComponent()
+      ..sprite = await loadSprite('player.jpg')
+      ..size = Vector2(50, 50)
+      ..position = size / 4;
+    add(player);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+    // 적 추가하기
+    enermy = SpriteComponent()
+      ..sprite = await loadSprite('enermy.png')
+      ..size = Vector2(50, 65)
+      ..position = size / 2;
+    add(enermy);
   }
 
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+  void onPanUpdate(DragUpdateInfo info) {
+    player.position.add(info.delta.global);
+  }
+
+  @override
+  KeyEventResult onKeyEvent(
+      KeyEvent event, Set<LogicalKeyboardKey> keysPressed) {
+    moveDirection = Vector2.zero();
+    double moveSpeed = 1.65;
+
+    if (keysPressed.contains(LogicalKeyboardKey.arrowLeft)) {
+      moveDirection.x = -moveSpeed;
+    }
+
+    if (keysPressed.contains(LogicalKeyboardKey.arrowRight)) {
+      moveDirection.x = moveSpeed;
+    }
+
+    if (keysPressed.contains(LogicalKeyboardKey.arrowUp)) {
+      moveDirection.y = -moveSpeed;
+    }
+
+    if (keysPressed.contains(LogicalKeyboardKey.arrowDown)) {
+      // 수정된 부분
+      moveDirection.y = moveSpeed;
+    }
+
+    return KeyEventResult.handled; // 반드시 결과값을 반환해야 함
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    // 캐릭터 이동 속도
+    player.position += moveDirection * 200 * dt;
   }
 }
