8f288be0f01d678a8140054995e2a04e67b9f115
wanni30 <hubhy@nate.com>
2025-03-03 20:11:09 +0900

    테스트 파일에 배경화면 추가하기

--- lib/test/test_world.dart
+++ lib/test/test_world.dart
@@ -1,12 +1,22 @@
 import 'package:flame/game.dart';
+import 'package:flame/components.dart';
+import 'package:flame/parallax.dart';
+import 'package:flutter/material.dart';
 import 'package:survivors_game/test/animation_image.dart';
-
-import 'package:survivors_game/test/animation_rectangle.dart';
 
 class TestWorld extends FlameGame {
   late AnimationImage animationImage;
+
   @override
   Future<void> onLoad() async {
+    // 배경 추가하기
+    final parallax = await loadParallaxComponent(
+      [ParallaxImageData('background.jpg')],
+      baseVelocity: Vector2(50, 0), // 천천히 스크롤되는 배경
+      repeat: ImageRepeat.repeat,
+    );
+    add(parallax);
+
     animationImage = AnimationImage(
       sprite: await loadSprite('enemy.png'),
       position: size / 2,
